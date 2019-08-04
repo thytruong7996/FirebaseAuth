272560395a4282d8b77c39bc7315981cdcb1ef04
thytruong7996 <49258442+thytruong7996@users.noreply.github.com>
2019-08-04 16:18:10 +0700

    update code

--- firebaseauth/src/app/app.component.ts
+++ firebaseauth/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'firebaseauth';
+  constructor(private afAuth: AngularFireAuth) {}
+
+  ngOnInit() {
+    this.afAuth.authState
+      .subscribe((user) => console.log(user));
+  }
+
+  googleSignInViaPopup() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    .then((userCredentials) => console.log(userCredentials));
+  }
+
+  googleSignInViaRedirect() {
+    this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider())
+    .then((userCredentials) => console.log(userCredentials));
+  }
+  
 }
