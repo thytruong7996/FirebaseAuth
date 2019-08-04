5b339a9f95ad30024cce1ab7788cc8b179ff3348
thytruong7996 <49258442+thytruong7996@users.noreply.github.com>
2019-08-04 14:38:20 +0700

    commit source

---
+++ firebaseauth/src/app/app-firebase/app-firebase.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from 'src/environments/environment';
+
+
+
+@NgModule({
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+  ],
+  exports: [
+    AngularFireModule,
+    AngularFireAuthModule
+  ]
+})
+export class AppFirebaseModule { }
